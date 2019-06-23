468ba8515e047d94bcf1f278de5b7120ff862a7f
naterush <narush@wharton.upenn.edu>
2019-06-23 18:39:01 -0400

    rpc call

--- hello-world.go
+++ hello-world.go
@@ -2,8 +2,47 @@ package main
 
 import (
     "fmt"
+    "net/http"
+    "encoding/json"
+    "bytes"
 )
 
+
+func test() {
+
+}
+
 func main() {
-    fmt.Println("Hi")
+    type Payload struct {
+        Jsonrpc string        `json:"jsonrpc"`
+        Method  string        `json:"method"`
+        Params  interface{} `json:"params"`
+        ID      int           `json:"id"`
+    }
+
+    data := Payload{
+        "2.0",
+        "eth_getBlockByNumber",
+        "0x1b4",
+        1,
+    }
+
+    payloadBytes, err := json.Marshal(data)
+    if err != nil {
+        return
+    }
+    body := bytes.NewReader(payloadBytes)
+
+    req, err := http.NewRequest("POST", "http://localhost:8545", body)
+    if err != nil {
+        return 
+    }
+    req.Header.Set("Content-Type", "application/json")
+
+    resp, err := http.DefaultClient.Do(req)
+    if err != nil {
+        return
+    }
+    defer resp.Body.Close()
+    fmt.Println("DONE")
 }
