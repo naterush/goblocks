7ad5bc0377a3d39d9ed9c232188b005575ef81bf
naterush <narush@wharton.upenn.edu>
2019-06-23 21:57:45 -0400

    update

--- hello-world.go
+++ hello-world.go
@@ -113,7 +113,7 @@ func main() {
     start := time.Now()
 
     // Number of blocks to process
-    numBlocks := 5000
+    numBlocks := 10000
 
     var blocks chan int
     blocks = make(chan int)
