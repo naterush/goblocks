afd56cf5a7b8f2cdb1fd433aeed2a917d05b7590
naterush <narush@wharton.upenn.edu>
2019-06-23 21:32:40 -0400

    update

--- hello-world.go
+++ hello-world.go
@@ -69,7 +69,7 @@ func getBlock(block int, res chan Result) {
 func main() {
     start := time.Now()
 
-    numBlocks := 1000
+    numBlocks := 2000
     res := make(chan Result)
 
     for i := 5000000; i < 5000000 + numBlocks; i++ {
