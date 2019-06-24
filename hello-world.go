87039b340f8cf401e8d79e63f46a9afbb22d65f9
naterush <narush@wharton.upenn.edu>
2019-06-23 21:47:53 -0400

    update

--- hello-world.go
+++ hello-world.go
@@ -25,62 +25,71 @@ type Result struct {
     body string
 }
 
-func getBlock(block int, res chan Result) {
+func blockProcessor(blocks chan int, res chan Result) {
+    // Process blocks untill the blocks channel closes
+    for block := range blocks {
+        hexBlockNum := fmt.Sprintf("0x%x", block)
 
-    hexBlockNum := fmt.Sprintf("0x%x", block)
-
-    data := Payload{
-        "2.0",
-        "eth_getBlockByNumber",
-        Params{hexBlockNum, false},
-        2,
+        data := Payload{
+            "2.0",
+            "eth_getBlockByNumber",
+            Params{hexBlockNum, false},
+            2,
+        }
+    
+        payloadBytes, err := json.Marshal(data)
+        if err != nil {
+            res <- Result{block, ""}
+            fmt.Println("Error:", err)
+            return
+        }
+    
+        body := bytes.NewReader(payloadBytes)
+    
+        req, err := http.NewRequest("POST", "http://localhost:8545", body)
+        if err != nil {
+            res <- Result{block, ""}
+            fmt.Println("Error:", err)
+            return 
+        }
+        req.Header.Set("Content-Type", "application/json")
+    
+        resp, err := http.DefaultClient.Do(req)
+    
+        if err != nil {
+            res <- Result{block, ""}
+            fmt.Println("Error:", err)
+            return
+        }
+    
+        defer resp.Body.Close()
+    
+        body1, err := ioutil.ReadAll(resp.Body)
+        res <- Result{block, string(body1)}
     }
-
-    payloadBytes, err := json.Marshal(data)
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-
-    body := bytes.NewReader(payloadBytes)
-
-    req, err := http.NewRequest("POST", "http://localhost:8545", body)
-    if err != nil {
-        fmt.Println("Error:", err)
-        return 
-    }
-    req.Header.Set("Content-Type", "application/json")
-
-    resp, err := http.DefaultClient.Do(req)
-
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-
-    defer resp.Body.Close()
-
-    body1, err := ioutil.ReadAll(resp.Body)
-    res <- Result{block, string(body1)}
-
-    //fmt.Println(string(body1))
 }
 
 func main() {
     start := time.Now()
 
+    // Number of blocks to process
     numBlocks := 1000
+
+    var blocks chan int
+    blocks = make(chan int)
+
     res := make(chan Result)
 
-    for i := 5000000; i < 5000000 + numBlocks; i++ {
-        go getBlock(i, res)    
+    // Make 500 dedicated block processors
+    for i := 0; i < 500; i++ {
+        go blockProcessor(blocks, res)
     }
 
+    // Set up the receiver
     var m map[int]string
     m = make(map[int]string)
     var wg sync.WaitGroup
-	wg.Add(1)
-
+    wg.Add(1)
     go func () {
         numReceived := 0
     
@@ -93,10 +102,32 @@ func main() {
             }
         }
     }()
+
+    for i := 5000000; i < 5000000 + numBlocks; i++ {
+        blocks <- i
+    }
+
     wg.Wait()
 
     elapsed := time.Since(start)
     fmt.Println("Concurrent took time:", elapsed)
+
+
+
+    // Have a channel we send blocks down -- that closes when all blocks have been sent
+    // Have a finite number of processes (about 1000)
+        // Gets the block from the RPC, and sends to the result channel
+        // Have a result reciever always chillin
+    
+    // How do we get out of a receiver into a map
+
+
+
+
+
+
+
+
     //fmt.Println(m)
 
 
