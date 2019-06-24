cb362b60da4f962761f23822c11755dd9ff66092
naterush <narush@wharton.upenn.edu>
2019-06-24 10:09:06 -0400

    update

---
+++ ipc.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "encoding/json"
+    "bytes"
+	"io/ioutil"
+	"net"
+)
+
+
+type Params []interface{}
+
+type Payload struct {
+    Jsonrpc string        `json:"jsonrpc"`
+    Method  string        `json:"method"`
+    Params                `json:"params"`
+    ID      int           `json:"id"`
+}
+
+type Result struct {
+    block int
+    body string
+}
+
+func getBlock(block int) string {
+    // Process blocks untill the blocks channel closes
+    hexBlockNum := fmt.Sprintf("0x%x", block)
+
+    data := Payload{
+        "2.0",
+        "eth_getBlockByNumber",
+        Params{hexBlockNum, false},
+        2,
+    }
+
+    payloadBytes, err := json.Marshal(data)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return ""
+    }
+
+    body := bytes.NewReader(payloadBytes)
+
+    req, err := http.NewRequest("POST", "http://localhost:8545", body)
+    if err != nil {
+        fmt.Println("Error:", err)
+        return ""
+    }
+    req.Header.Set("Content-Type", "application/json")
+
+    resp, err := http.DefaultClient.Do(req)
+
+    if err != nil {
+        fmt.Println("Error:", err)
+        return ""
+    }
+
+    defer resp.Body.Close()
+
+    body1, err := ioutil.ReadAll(resp.Body)
+    return string(body1)
+}
+
+func main() {
+	l, err := net.Listen("unix", "~/.local/share/io.ethereum.parity/jsonrpc.ipc")
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	fmt.Println("HERE", l)
+
+}
