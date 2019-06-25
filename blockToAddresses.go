fa039db5d9c5e22d25cb736256bbb93a2c4301c8
naterush <narush@wharton.upenn.edu>
2019-06-24 21:34:03 -0400

    update

--- blockToAddresses.go
+++ blockToAddresses.go
@@ -62,7 +62,7 @@ func getAddress(traces chan []byte, done chan int) {
         }
 
         for i :=0; i<len(traces.Result); i++ {
-            fmt.Println("Trace:", traces.Result[i].callType)
+            fmt.Println("Trace:", traces.Result[i].CallType)
         }
         done <- 1
     }
