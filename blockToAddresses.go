c540935a26b6c76d8f086fb4c58215c4b2bd162f
naterush <narush@wharton.upenn.edu>
2019-06-25 14:37:32 -0400

    update

--- blockToAddresses.go
+++ blockToAddresses.go
@@ -169,7 +169,7 @@ func getAddress(traceAndLogs chan TraceAndLogs) {
                 hit++
 
                 if (hit > 1) {
-                    fmt.Println(string(blockNum))
+                    fmt.Println(string(blockTraceAndLog.Traces))
                 }
                 // if it's a reward, add the miner
                 author := traces.Result[i].Action.Author
