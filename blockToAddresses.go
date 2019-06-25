b0d34e85e56fdb3c034403ca7a25693584dcc6e6
naterush <narush@wharton.upenn.edu>
2019-06-25 13:18:06 -0400

    update

--- blockToAddresses.go
+++ blockToAddresses.go
@@ -229,7 +229,26 @@ func getTrace(blocks chan int, traces chan []byte) {
     }
 }
 
+func testSearch() {
+    fileNames := make(chan string)
+    sightings := make(chan AddrSighting)
+
+    for i := 0; i < 10; i++ {
+        go searchForAddress("0xe3e1d847f4d369faa89b01393b34a8193da6dead", fileNames, sightings)
+    }
+
+    for i := 6000000; i < 6000000 + 10000; i++ {
+        fileName := leftZero(strconv.Itoa(i), 9) + ".txt"
+        fileNames <- fileName
+    }
+
+    done := make(chan int)
+    <- done */
+}
+
 func main() {
+    testSearch()
+    /*
     startBlock := 6000000
     numBlocks := 10000
 
@@ -251,5 +270,5 @@ func main() {
     
     // blah, just wait around for ever (have to manuall terminate the process...)
     done := make(chan int)
-    <- done
+    <- done */
 }
