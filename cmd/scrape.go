497b9bba57134c97b004ceb66a9d9f802c01009d
naterush <naterush1997@gmail.com>
2019-08-05 15:31:22 -0400

    make parallel

--- cmd/scrape.go
+++ cmd/scrape.go
@@ -284,12 +284,10 @@ func extractAddresses(rpcProvider string, addressChannel chan BlockInternals, ad
 			startIdx = endIdx
 		}
 		close(rangeChannelLogs)
-		fmt.Println("\n\nWAITING FOR TRACES AND LOGS")
+		
 		// Wait for the various state machines to finish
 		traceWG.Wait()
-		fmt.Println("TRACES FINISHED")
 		logWG.Wait()
-		fmt.Println("LOGS FINISHED")
 		// Close the address channel, to notify the address receiver all have been sent
 		close(addressChannel)
 		// wait for the address receiver to write out the file
